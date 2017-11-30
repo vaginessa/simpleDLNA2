99fc412171f17ab341209e4d68cf4cd2c0e9f519
nono303 <github@nono303.net>
2017-11-30 13:28:27 +0100

    add datetime to log message

--- sdlna/Options.cs
+++ sdlna/Options.cs
@@ -132,7 +132,7 @@ namespace NMaier.SimpleDlna
       var appender = new ConsoleAppender();
       var layout = new PatternLayout
       {
-        ConversionPattern = "%6level [%3thread] %-20.20logger{1} - %message%newline%exception"
+        ConversionPattern = "%date %6level [%3thread] %-20.20logger{1} - %message%newline%exception"
       };
       layout.ActivateOptions();
       appender.Layout = layout;
